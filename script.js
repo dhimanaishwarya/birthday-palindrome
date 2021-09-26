3b5d070ad7a321c59f24dd6351d0a372ed8e183f
aiaishwa <aishwarya.dhiman@nokia.com>
2021-09-26 16:32:16 +0530

    added excercises

--- script.js
+++ script.js
@@ -25,7 +25,52 @@ function isPalindrome(str) {
 console.log(isPalindrome("madam"));
 console.log(isPalindrome("aishwarya"));
 
+let date = {
+    day: 26,
+    month: 9,
+    year: 2020
+}
 
+function convertDatetoStr(date) {
+
+    let dateStr = {
+        day: '',
+        month: '',
+        year: ''
+    }
+
+    if (date.day < 10) {
+        dateStr.day = "0" + date.day;
+    } else {
+        dateStr.day = date.day.toString();
+    }
+
+    if (date.month < 10) {
+        dateStr.month = "0" + date.month;
+    } else {
+        dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+    return dateStr;
+}
+
+// return String(date.day) + String(date.month) + String(date.year);
+
+console.log(convertDatetoStr(date));
+
+function getDateFormats(date) {
+    let dateStr = convertDatetoStr(date);
+
+    let ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    let mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    let yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    let ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    let mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    let yyddmm = dateStr.year.slice(-2) + dateStr.day + dateStr.month;
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
+}
 
 
 
